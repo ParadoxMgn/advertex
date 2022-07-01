efff1a5490aceef6bd2cafd9a7367185291e93bc
Eugene Shumkov <radoxmgn@gmail.com>
2022-07-01 20:26:42 +0300

    change no-webp

--- gulp/tasks/scss.js
+++ gulp/tasks/scss.js
@@ -10,56 +10,56 @@ const sass = gulpSass(dartSass);
 
 export const scss = () => {
   return app.gulp.src(app.path.src.scss, { sourcemaps: app.isDev })
-  .pipe(
-    app.plugins.plumber(
-      app.plugins.notify.onError({
-        title: "HTML",
-        message: "Error: <%= error.message %>"
-      })
+    .pipe(
+      app.plugins.plumber(
+        app.plugins.notify.onError({
+          title: "HTML",
+          message: "Error: <%= error.message %>"
+        })
+      )
     )
-  )
-  .pipe(app.plugins.replace(/@img\//g, '../img/'))
-  .pipe(sass({
-    outputStyle: 'expanded'
-  }))
+    .pipe(app.plugins.replace(/@img\//g, '../img/'))
+    .pipe(sass({
+      outputStyle: 'expanded'
+    }))
 
-  .pipe(
-    app.plugins.if(
-      app.isBuild,
-      groupCssMediaQueries()
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        groupCssMediaQueries()
+      )
     )
-  )
-  .pipe(app.plugins.replace(/@img\//g, 'img/'))
-  /* .pipe(
-    app.plugins.if(
-      app.isBuild,
-      webpcss({
-        webpClass: ".webp",
-        noWebpClass: ".no-webp"
-      })
+    .pipe(app.plugins.replace(/@img\//g, 'img/'))
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        webpcss({
+          webpClass: ".webp",
+          noWebpClass: ".no-webp"
+        })
+      )
     )
-  ) */
-  .pipe(
-    app.plugins.if(
-      app.isBuild,
-      autoprefixer({
-        grid: true,
-        overrideBrowserslist: ["last 5 versions"],
-        cascade: true
-      })
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        autoprefixer({
+          grid: true,
+          overrideBrowserslist: ["last 5 versions"],
+          cascade: true
+        })
+      )
     )
-  )
-  // Раскоментировать если нужен не сжатый дубль файла стилей
-  .pipe(app.gulp.dest(app.path.build.css))
-  .pipe(
-    app.plugins.if(
-      app.isBuild,
-      cleanCss()
+    // Раскоментировать если нужен не сжатый дубль файла стилей
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(
+      app.plugins.if(
+        app.isBuild,
+        cleanCss()
+      )
     )
-  )
-  .pipe(rename({
-    extname: ".min.css"
-  }))
-  .pipe(app.gulp.dest(app.path.build.css))
-  .pipe(app.plugins.browsersync.stream());
+    .pipe(rename({
+      extname: ".min.css"
+    }))
+    .pipe(app.gulp.dest(app.path.build.css))
+    .pipe(app.plugins.browsersync.stream());
 }
